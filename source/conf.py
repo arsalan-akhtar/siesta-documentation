17ead599c3f11e12ec54f7eb3563bcca1a0ceeb7
arsalan-akhtar <arsalan_akhtar@outlook.com>
2020-01-23 12:37:44 +0100

    6

--- source/conf.py
+++ source/conf.py
@@ -53,3 +53,20 @@ html_theme ='default'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+
+
+
+
+
+
+
+
+#============================
+mathjax_config = {
+    'TeX': {
+        'Macros': {
+            'br': '{\\mathbf r}',
+            'bk': '{\\mathbf k}',
+            'bG': '{\\mathbf G}'}}}
