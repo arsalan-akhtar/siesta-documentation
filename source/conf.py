15e9df67f25218e5f34476d8081735bf064d88bb
arsalan-akhtar <arsalan_akhtar@outlook.com>
2020-01-23 12:50:23 +0100

    6

--- source/conf.py
+++ source/conf.py
@@ -69,13 +69,6 @@ html_title = 'GPAW'
 html_last_updated_fmt = '%a, %d %b %Y %H:%M:%S'
 dev_version = '19.8.2b1'  # This line auto-edited by newrelease script
 stable_version = '19.8.1'  # This line auto-edited by newrelease script
-html_context = {
-    'current_version': __version__,
-    'versions':
-        [('{} (development)'.format(dev_version),
-          'https://wiki.fysik.dtu.dk/gpaw/dev'),
-         ('{} (latest stable)'.format(stable_version),
-          'https://wiki.fysik.dtu.dk/gpaw')]}
 
 mathjax_config = {
     'TeX': {
