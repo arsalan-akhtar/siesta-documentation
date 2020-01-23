4f71a968036fff07bbf14cf8cbd0e3393f7e20e4
arsalan-akhtar <arsalan_akhtar@outlook.com>
2020-01-23 12:34:19 +0100

    6

--- source/conf.py
+++ source/conf.py
@@ -30,8 +30,7 @@ master_doc = 'index'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['images',
-              'sphinx.ext.mathjax',
+extensions = ['sphinx.ext.mathjax'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
